cb2355944b6bff05a753a0be7ca297f731230848
Степан Кирсанов <82017654+Oxidante@users.noreply.github.com>
2022-05-20 20:04:17 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-// DARK MODE TOGGLE
-document.querySelector('.dark-mode-switch').onclick = () => {
-	document.querySelector('body').classList.toggle('dark')
-	document.querySelector('body').classList.toggle('light')
-}
 
-// CHECK LEAP YEAR
+let calendar = document.querySelector('.calendar')
+
+const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
 isLeapYear = (year) => {
 	return (year % 4 === 0 && year % 100 !== 0 && year % 400 !== 0) || (year % 100 === 0 && year % 400 === 0)
 }
@@ -13,26 +11,26 @@ getFebDays = (year) => {
 	return isLeapYear(year) ? 29 : 28
 }
 
-let calendar = document.querySelector('.calendar')
-
-const month_names = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-let month_picker = document.querySelector('#month-picker')
-// GENERATE CALENDAR
-
 generateCalendar = (month, year) => {
-	let calendar_days = document.querySelector('.calendar-days')
-	calendar_days.innerHTML - ''
-	let calendar_header_year = document.querySelector('#year')
+
+	let calendar_days = calendar.querySelector('.calendar-days')
+	let calendar_header_year = calendar.querySelector('#year')
 
 	let days_of_month = [31, getFebDays(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-	let currDate = new Date()
+	calendar_days.innerHTML = ''
 
-	month_picker.innerHTML = month_names[month]
+	let currDate = new Date()
+	if (!month) month = currDate.getMonth()
+	if (!year) year = currDate.getFullYear()
+
+	let curr_month = `${month_names[month]}`
+	month_picker.innerHTML = curr_month
 	calendar_header_year.innerHTML = year
 
-	let first_day = new Date(month, year, 1)
+	// get first day of month
+
+	let first_day = new Date(year, month, 1)
 
 	for (let i = 0; i <= days_of_month[month] + first_day.getDay() - 1; i++) {
 		let day = document.createElement('div')
@@ -40,9 +38,9 @@ generateCalendar = (month, year) => {
 			day.classList.add('calendar-day-hover')
 			day.innerHTML = i - first_day.getDay() + 1
 			day.innerHTML += `<span></span>
-									<span></span>
-									<span></span>
-									<span></span>`
+                           <span></span>
+                           <span></span>
+                           <span></span>`
 			if (i - first_day.getDay() + 1 === currDate.getDate() && year === currDate.getFullYear() && month === currDate.getMonth()) {
 				day.classList.add('curr-date')
 			}
@@ -51,9 +49,45 @@ generateCalendar = (month, year) => {
 	}
 }
 
+let month_list = calendar.querySelector('.month-list')
+
+month_names.forEach((e, index) => {
+	let month = document.createElement('div')
+	month.innerHTML = `<div data-month="${index}">${e}</div>`
+	month.querySelector('div').onclick = () => {
+		month_list.classList.remove('show')
+		curr_month.value = index
+		generateCalendar(index, curr_year.value)
+	}
+	month_list.appendChild(month)
+})
+
+let month_picker = calendar.querySelector('#month-picker')
+
+month_picker.onclick = () => {
+	month_list.classList.add('show')
+}
+
 let currDate = new Date()
 
 let curr_month = { value: currDate.getMonth() }
 let curr_year = { value: currDate.getFullYear() }
 
 generateCalendar(curr_month.value, curr_year.value)
+
+document.querySelector('#prev-year').onclick = () => {
+	--curr_year.value
+	generateCalendar(curr_month.value, curr_year.value)
+}
+
+document.querySelector('#next-year').onclick = () => {
+	++curr_year.value
+	generateCalendar(curr_month.value, curr_year.value)
+}
+
+let dark_mode_toggle = document.querySelector('.dark-mode-switch')
+
+dark_mode_toggle.onclick = () => {
+	document.querySelector('body').classList.toggle('light')
+	document.querySelector('body').classList.toggle('dark')
+}
